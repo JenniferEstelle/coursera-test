08c8d3b5af9fd04b551ddf9d29b74bb544cc4418
Jenny Bedford <jen@jenniferestelle.com>
2017-02-12 15:57:23 -0500

    Yup

--- module2/app.js
+++ module2/app.js
@@ -3,20 +3,48 @@
 
     angular.module('ShoppingListCheckOff', [])
         .controller('AlreadyBoughtController', AlreadyBoughtController)
-        .controller('ToBuyController', ToBuyController);  //define viewModel of view
+        .controller('ToBuyController', ToBuyController)  //define viewModel of view
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-//apply properties directly to the instance of the controller itself.  Angular JS will bind this instance of the controller to $scope behind the scenes. 
-    function ToBuyController () {
-        console.log("controller exists!")
-        var buy = this;
-        this.title = "buy me!"
+    //apply properties directly to the instance of the controller itself.  Angular JS will bind this instance of the controller to $scope behind the scenes. 
+    ToBuyController.inject['ShoppingListCheckOffService'];
+    AlreadyBoughtController.inject['ShoppingListCheckOffService'];
+
+    function ToBuyController(ShoppingListCheckOffService) {
+        console.log("controller exists!");
+        var buyCtrl = this;
+        this.item = ShoppingListCheckOffService();
     };
 
-    function AlreadyBoughtController() {
-        console.log("controller exists!")
-        var bought = this;
-        this.title = "bought!"
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        console.log("controller exists!");
+        var boughtCtrl = this;  //you are not changing the value or assigning this to boughtCtrl here.  Readability is the purpose here.
+        
+
     };
+
+    function ShoppingListCheckOffService () {
+        console.log("test!");
+
+        
+
+        //method that removes item from tobuy array and pushes to the bought array
+        var service = this;
+        this.bought = [];
+        this.tobuy = [];
+
+        service.markBought = function () {
+            console.log("service.buyItem is working");
+            //add item to bought array
+            //remove item from tobuy array 
+            
+        };
+
+        
+
+
+
+};
 
 
 })();
