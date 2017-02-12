c0a10a995ae14584d43a34e4c46cc30053aee929
Jennifer Bedford <jennifer.e.bedford@gmail.com>
2017-02-12 16:15:14 -0500

    baseline

--- module2/app.js
+++ module2/app.js
@@ -7,44 +7,41 @@
         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
     //apply properties directly to the instance of the controller itself.  Angular JS will bind this instance of the controller to $scope behind the scenes. 
-    ToBuyController.inject['ShoppingListCheckOffService'];
-    AlreadyBoughtController.inject['ShoppingListCheckOffService'];
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
     function ToBuyController(ShoppingListCheckOffService) {
         console.log("controller exists!");
         var buyCtrl = this;
-        this.item = ShoppingListCheckOffService();
+
     };
 
     function AlreadyBoughtController(ShoppingListCheckOffService) {
         console.log("controller exists!");
         var boughtCtrl = this;  //you are not changing the value or assigning this to boughtCtrl here.  Readability is the purpose here.
-        
+
 
     };
 
-    function ShoppingListCheckOffService () {
-        console.log("test!");
-
-        
-
-        //method that removes item from tobuy array and pushes to the bought array
-        var service = this;
-        this.bought = [];
-        this.tobuy = [];
-
-        service.markBought = function () {
-            console.log("service.buyItem is working");
-            //add item to bought array
-            //remove item from tobuy array 
-            
-        };
-
-        
+    function ShoppingListCheckOffService() {
+        // console.log("test!");
 
 
 
-};
+        // //method that removes item from tobuy array and pushes to the bought array
+        // var service = this;
+        // this.bought = [];
+        // this.tobuy = [];
+
+        // service.markBought = function () {
+        //     console.log("service.buyItem is working");
+        //     //add item to bought array
+        //     //remove item from tobuy array 
+
+        // };
+
+
+    };
 
 
 })();
